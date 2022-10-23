794d7b0109c31afd2125f3cd16ce226cb8d182ec
Brendan Tuckerman <brendan.tuckerman@gmail.com>
2022-10-24 10:12:51 +1100

    updated urls for deploy

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const server = http.createServer(app)
 const { Server } = require('socket.io');
 const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: "https://mrmaverick79.github.io/good-point-vue-frontend/",
       methods: "*",
       credentials: true
     }
