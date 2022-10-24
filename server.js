4b095f4b7892bc196cd8adab481038fe74134d32
Brendan Tuckerman <brendan.tuckerman@gmail.com>
2022-10-24 12:31:20 +1100

    Updated Seeds

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const server = http.createServer(app)
 const { Server } = require('socket.io');
 const io = new Server(server, {
     cors: {
-      origin:"https://cryptic-lake-39023.herokuapp.com/socket.io/",
+      origin:"https://cryptic-lake-39023.herokuapp.com/socket.io/*",
       methods: "*",
       credentials: true,
       pingTimeout: 7000,
